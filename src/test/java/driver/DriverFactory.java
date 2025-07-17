5a6b94d0c0765221df83f7770bcb0f28272e07e2
jygonzalez <jorge.gonzalezalcoce@ucalgary.ca>
2025-07-16 21:21:19 -0600

    Add DriverFactory class

---
+++ src/test/java/driver/DriverFactory.java
@@ -0,0 +1,48 @@
+package driver;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import constants.FrameworkConstants;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public final class DriverFactory {
+
+	private DriverFactory() {
+
+	}
+
+	public static WebDriver createDriver(String browserName) {
+
+		WebDriver driver = null;
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if (browserName.equalsIgnoreCase("safari")) {
+			WebDriverManager.safaridriver().setup();
+			driver = new SafariDriver();
+		} else {
+
+		}
+
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(FrameworkConstants.WAIT_IMPLICIT_TIMEOUT));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(FrameworkConstants.WAIT_PAGE_LOAD_TIMEOUT));
+
+		return driver;
+	}
+
+}
