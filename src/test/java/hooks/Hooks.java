1685264112bf69d3e631c231d7510088f17b9d09
jygonzalez <jorge.gonzalezalcoce@ucalgary.ca>
2025-07-16 21:22:39 -0600

    Rename CucumberHooks to Hooks. Begin work on hooks

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,26 @@
+package hooks;
+
+import org.openqa.selenium.WebDriver;
+
+import constants.FrameworkConstants;
+import driver.DriverFactory;
+import driver.DriverManager;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+	@Before
+	public void beforeScenario() {
+
+		WebDriver driver = DriverFactory.createDriver(FrameworkConstants.BROWSER);
+
+		DriverManager.setDriver(driver);
+		DriverManager.getDriver().get(FrameworkConstants.URL);
+	}
+
+	@After
+	public void afterScenario() {
+		DriverManager.quit();
+	}
+}
