ce978ecc145887e8783c7f09f4a156788a062849
jygonzalez <jorge.gonzalezalcoce@ucalgary.ca>
2025-07-19 17:58:33 -0600

    Implement the step definition class which defines the steps we have so far in our feature files

--- src/test/java/stepdefinitions/CarbohydrateCalculator.java
+++ src/test/java/stepdefinitions/CarbohydrateCalculator.java
@@ -1,13 +1,34 @@
 package stepdefinitions;
 
+import constants.FrameworkConstants;
+import context.ScenarioContext;
+import driver.DriverManager;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import pages.CarbohydrateCalculatorPage;
 
-public class SearchOwner {
-	
-	@Given("A clean environment")
-	public void a_clean_environment() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+public class CarbohydrateCalculator {
+
+	private final ScenarioContext context;
+	private CarbohydrateCalculatorPage carbohydrateCalculatorPage;
+
+	public CarbohydrateCalculator(ScenarioContext context) {
+		this.context = context;
 	}
-	
+
+	@Given("User navigates to Home page")
+	public void user_navigates_to_home_page() {
+		DriverManager.getDriver().get(FrameworkConstants.URL);
+	}
+
+	@Given("User navigates to Carbohydrate Calculator page")
+	public void user_navigates_to_carbohydrate_calculator_page() {
+		DriverManager.getDriver().get(FrameworkConstants.URL + "/carbohydrate-calculator");
+		carbohydrateCalculatorPage = new CarbohydrateCalculatorPage(DriverManager.getDriver());
+	}
+
+	@When("User enters age as {string}")
+	public void user_enters_age_as(String age) {
+		carbohydrateCalculatorPage.enterAge(age);
+	}
 }
