e7500a7baedd217177d1ad56ec136914b8286fe1
jygonzalez <jorge.gonzalezalcoce@ucalgary.ca>
2025-07-29 21:44:25 -0600

    Rename package from runner to runners. Add plugins for allure reporting and better logging (pretty)

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,4 +1,4 @@
-package runner;
+package runners;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -7,7 +7,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @Test
-@CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinitions", "hooks", "context" })
+@CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinitions", "hooks", "context" }, plugin = {
+		"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json",
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 	@Override
